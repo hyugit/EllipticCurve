467b41b00e4486e325aceb3b299e7e94f66cbfaa
mryu87 <13056774+mryu87@users.noreply.github.com>
2018-01-09 13:39:50 -0500

    remove associatedtype multiplier

--- Sources/FiniteField.swift
+++ Sources/FiniteField.swift
@@ -5,12 +5,11 @@ import UInt256
 
 public protocol FiniteField: Equatable, CustomStringConvertible {
     associatedtype Element: UnsignedInteger, FixedWidthInteger
-    associatedtype Multiplier where Multiplier: FiniteField
 
     static var Characteristic: Element { get }
     static var Order: Element { get }
     static var Zero: Self { get }
 
     static func +(lhs: Self, rhs: Self) -> Self
-    static func *(lhs: Multiplier, rhs: Self) -> Self
+    static func *<T>(lhs: T, rhs: Self) -> Self where T: Numeric
 }
