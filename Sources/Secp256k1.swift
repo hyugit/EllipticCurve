0e54a3340546680abc0dd783637e72be6e60ebc5
hyugit <13056774+hyugit@users.noreply.github.com>
2018-01-12 19:03:55 -0800

    update secp256k1 to relfect protocol changes

--- Sources/Secp256k1.swift
+++ Sources/Secp256k1.swift
@@ -3,24 +3,22 @@
 import Foundation
 import UInt256
 
-public struct Parameters {
-    public static var P = UInt256([UInt64.max, UInt64.max, UInt64.max, 0xFFFFFFFEFFFFFC2F])
+public let Parameters = (
+    P: UInt256([UInt64.max, UInt64.max, UInt64.max, 0xFFFFFFFEFFFFFC2F]),
 
-    public static var a = FFInt(0)
-    public static var b = FFInt(7)
+    a: FFInt256(0),
+    b: FFInt256(7),
 
-    static var Gx = UInt256([0x79BE667EF9DCBBAC, 0x55A06295CE870B07, 0x029BFCDB2DCE28D9, 0x59F2815B16F81798])
-    static var Gy = UInt256([0x483ADA7726A3C465, 0x5DA4FBFC0E1108A8, 0xFD17B448A6855419, 0x9C47D08FFB10D4B8])
+    G: (
+        x: UInt256([0x79BE667EF9DCBBAC, 0x55A06295CE870B07, 0x029BFCDB2DCE28D9, 0x59F2815B16F81798]),
+        y: UInt256([0x483ADA7726A3C465, 0x5DA4FBFC0E1108A8, 0xFD17B448A6855419, 0x9C47D08FFB10D4B8])
+    ),
 
-    public static var G = ECPoint(x: FFInt(Gx), y: FFInt(Gy))
+    n: UInt256([UInt64.max, 0xFFFFFFFFFFFFFFFE, 0xBAAEDCE6AF48A03B, 0xBFD25E8CD0364141])
+)
 
-    public static var n = UInt256([UInt64.max, 0xFFFFFFFFFFFFFFFE, 0xBAAEDCE6AF48A03B, 0xBFD25E8CD0364141])
-}
-
-public struct FFInt: FiniteField {
-    public typealias Element = UInt256
+public struct FFInt256: FiniteFieldInteger {
     public static var Characteristic: UInt256 = Parameters.P
-    public static var Order: UInt256 = Parameters.n
     public var value: UInt256
 
     public init() {
@@ -28,17 +26,18 @@ public struct FFInt: FiniteField {
     }
 }
 
-public struct ECPoint: EllipticCurve {
-    public typealias Coordinate = FFInt
+public struct ECPoint: EllipticCurveOverFiniteField {
+    public static var Generator = ECPoint(withCoordinates: Parameters.G)
 
-    public static var a: Coordinate = Parameters.a
-    public static var b: Coordinate = Parameters.b
+    public static var Order: UInt256 = Parameters.n
+    public static var a = Parameters.a
+    public static var b = Parameters.b
 
-    public var x: Coordinate
-    public var y: Coordinate?
+    public var _value: UInt256?
+    public var x: FFInt256
+    public var y: FFInt256?
 
     public init() {
         x = 0
-        y = nil
     }
 }
