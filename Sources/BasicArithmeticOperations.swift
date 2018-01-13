7ad833fec98d7afc17d5c40d5be5d05274eee27f
hyugit <13056774+hyugit@users.noreply.github.com>
2018-01-12 19:58:26 -0800

    add readme, travis.yml, update deps.

--- Sources/BasicArithmeticOperations.swift
+++ Sources/BasicArithmeticOperations.swift
@@ -5,4 +5,6 @@ import Foundation
 public protocol BasicArithmeticOperations: Numeric, Comparable {
     static func /(lhs: Self, rhs: Self) -> Self
     static func /=(lhs: inout Self, rhs: Self)
+
+    // TODO: maybe add following func requirements: add, sub, mult, div
 }
