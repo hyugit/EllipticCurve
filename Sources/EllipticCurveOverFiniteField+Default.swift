f4cf9f5056c54ed64fca5888f7de4f57a0c9711e
hyugit <13056774+hyugit@users.noreply.github.com>
2018-01-13 05:56:40 -0800

    remove required `value` property from ECOverFF.
    To guard seed value range, use init?(withSeed:)

--- Sources/EllipticCurveOverFiniteField+Default.swift
+++ Sources/EllipticCurveOverFiniteField+Default.swift
@@ -24,45 +24,21 @@ extension EllipticCurveOverFiniteField {
         }
     }
 
-    public var value: Element? {
-        get {
-            return _value
-        }
-        set {
-            guard newValue != nil else {
-                _value = nil
-                x = 0
-                y = nil
-                return
-            }
-
-            guard Self.Element(0) < newValue! && newValue! <= Self.Order else {
-                _value = nil
-                x = 0
-                y = nil
-                return
-            }
-
-            _value = newValue!
-            let P = newValue! * Self.Generator
-            x = P.x
-            y = P.y
-        }
-    }
-
     public init(withCoordinates coord: (x: Element, y: Element)) {
         self.init(x: Coordinate(withValue: coord.x), y: Coordinate(withValue: coord.y))
     }
 
-    public init?(withValue source: Element) {
-        guard 0 < source && source <= Self.Order else {
+    public init?(withSeed value: Element) {
+        guard 0 < value && value <= Self.Order else {
             return nil
         }
         self.init()
-        self.value = source
+        let P = value * Self.Generator
+        self.x = P.x
+        self.y = P.y
     }
 
     public init?(_ source: Element) {
-        self.init(withValue: source)
+        self.init(withSeed: source)
     }
 }
