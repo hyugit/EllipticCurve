c178af1cd46d6462161299ab0b63685e4d675178
hyugit <13056774+hyugit@users.noreply.github.com>
2018-01-14 12:06:48 -0800

    cleanups

--- Tests/Secp256k1Tests.swift
+++ Tests/Secp256k1Tests.swift
@@ -9,3 +9,39 @@ class Secp256k1Tests: XCTestCase {
         XCTAssertNotNil(Secp256k1.Generator)
     }
 }
+
+class Secp256r1Tests: XCTestCase {
+    func testInit() {
+        XCTAssertNotNil(Secp256r1())
+        XCTAssertNotNil(Secp256r1.Generator)
+    }
+}
+
+class Secp224k1Tests: XCTestCase {
+    func testInit() {
+        XCTAssertNotNil(Secp224k1())
+        XCTAssertNotNil(Secp224k1.Generator)
+    }
+}
+
+class Secp224r1Tests: XCTestCase {
+    func testInit() {
+        XCTAssertNotNil(Secp224r1())
+        XCTAssertNotNil(Secp224r1.Generator)
+    }
+}
+
+class Secp192k1Tests: XCTestCase {
+    func testInit() {
+        XCTAssertNotNil(Secp192k1())
+        XCTAssertNotNil(Secp192k1.Generator)
+    }
+}
+
+class Secp192r1Tests: XCTestCase {
+    func testInit() {
+        XCTAssertNotNil(Secp192r1())
+        XCTAssertNotNil(Secp192r1.Generator)
+    }
+}
+
