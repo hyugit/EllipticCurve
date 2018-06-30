433c26e00a49c6c24f4cefd4558e0e709edb6f0b
Sajjon <alex.cyon@gmail.com>
2018-06-30 12:38:06 +0200

    Making project compile under Xcode 94

--- Sources/FiniteField/FiniteFieldInteger.swift
+++ Sources/FiniteField/FiniteFieldInteger.swift
@@ -6,7 +6,7 @@ import UInt256
 /// FiniteFieldInteger: a finite field with an integer as element type.
 /// Arithmetic are implemented by default for UInt family members that
 /// follow UnsignedInteger & FixedWidthInteger protocols
-public protocol FiniteFieldInteger: FiniteField, BasicArithmeticOperations where Element: FixedWidthIntegerWithBarrettDivision {
+public protocol FiniteFieldInteger: FiniteField, BasicArithmeticOperations where Element: FixedWidthIntegerWithBarrettDivision, IntegerLiteralType == Int, Magnitude == Self {
 
     var value: Element { get set }
 
